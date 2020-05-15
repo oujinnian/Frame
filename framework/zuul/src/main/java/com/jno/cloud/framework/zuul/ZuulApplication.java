a617826f1cf3a3ce8466037257c25654d254802e
oujinnian <286747694@qq.com>
2020-05-15 15:48:12 +0800

    加入redis配置类 加入redis工具类

--- framework/zuul/src/main/java/com/jno/cloud/framework/zuul/ZuulApplication.java
+++ framework/zuul/src/main/java/com/jno/cloud/framework/zuul/ZuulApplication.java
@@ -1,16 +1,24 @@
 package com.jno.cloud.framework.zuul;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
 import org.springframework.context.annotation.ComponentScan;
 
+@EnableZuulServer
 @EnableZuulProxy
-@SpringBootApplication
 @EnableDiscoveryClient
 //@EnableAuthClient
 @ServletComponentScan
+@SpringBootApplication
 @ComponentScan(basePackages = {"com.jno.cloud.framework.redis","com.jno.cloud.framework.zuul"})
 public class ZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class,args);
+    }
+
 }
